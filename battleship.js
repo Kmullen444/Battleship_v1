bb6d7ad4d6bdc90d0792ccae88a0de879a141535
kmullen444 <kmullen444@gmail.com>
2020-08-04 20:20:22 -0700

    finished

--- battleship.js
+++ battleship.js
@@ -8,14 +8,27 @@ var guesses = 0;
 
 var isSunk = false;
 
-while (isSunk == false){
-    guess = prompt("Ready,aim, fire! (enter a number fomr 0-6):");
-    if (guess < 0 || guess > 6){
-        alert("Please enter a valid number!");
-    }else{
+while (isSunk == false) {
+    guess = prompt("Ready, aim, fire! (enter a number from 0-6)");
+    if (guess < 0 || guess > 6) {
+        alert("Plese anter a vaild cell number!");
+    }else {
         guesses = guesses + 1;
-    }
     
+        if (guess == location1 || guess == location2 || guess == location3) {
+            alert("Hit!");
+            hits = hits + 1;
+            if (hits == 3) {
+                isSunk = true;
+                alert("You sank my battleship!");
+            }
+        }else {
+            alert("Miss!");
+        }
+    
+    }
 }
+var stats = "You took " + guesses + " guesses to sink the battleship, " +
+"which means you shooting accuracy was " + (3/guesses);
 
-
+alert(stats);
